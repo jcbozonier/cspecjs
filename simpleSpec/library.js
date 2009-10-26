1851b7e776155a0945e5c6d0926c64690fb20de0
Justin Bozonier <darkxanthos@gmail.com>
2009-10-25 21:31:01 -0700

    updated with more tests to see how it looks

--- simpleSpec/library.js
+++ simpleSpec/library.js
@@ -6,6 +6,7 @@ var context = null;
 var currentShould = null;
 var because = null;
 var failure = false;
+var anyFailures = false;
 
 var should = function(label)
 {
@@ -13,26 +14,55 @@ var should = function(label)
 	currentShouldLabel = label;
 }
 
-var assertNotNull = function(value)
+var specReporter = function(boolValue)
 {
-	if(value == null)
+	if(boolValue)
 	{
-		document.write("FAILED when " + currentContextLabel + " it should " + currentShouldLabel);
-		failure = true;
-	} else {
 		document.write("PASSED when " + currentContextLabel + " it should " + currentShouldLabel + "<br />");
+	} else {
+		document.write("FAILED when " + currentContextLabel + " it should " + currentShouldLabel + "<br />");
+		failure = true;
+		anyFailures = true;
 	}
 }
+
+var assertNotNull = function(value)
+{
+	specReporter(value != null);
+}
+
+var assertAreEqual = function(value1, value2)
+{
+	failure = false;
+	specReporter(value1 == value2);
+}
+
+var assertGreaterThanZero = function(value)
+{
+	failure = false;
+	specReporter(value > 0);
+}
+
+var assertLessThanZero = function(value)
+{
+	failure = false;
+	specReporter(value < 0);
+}
+
 
 function runTests(){
 	for (var testName in when) {
 		currentContextLabel = testName;
+		document.write("<br />");
 		when[testName]();
-		if(failure)
-		{
-			return;
-		}
 	}
 	
-	document.write("All tests PASS!");
+	if(anyFailures)
+	{
+		document.write("Tests FAILED!");
+	}
+	else{
+		document.write("All tests PASS!");
+	}
+	
 }
